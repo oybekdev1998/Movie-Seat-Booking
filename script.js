3cae5997562de137c12aac919d2ca0e4deb29ee1
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2021-12-28 11:26:31 +0500

    Created localStorage and creted populate function

--- script.js
+++ script.js
@@ -6,23 +6,57 @@ const count = document.getElementById('count')
 const total = document.getElementById('total')
 const movieSelect = document.getElementById('movie')
 
+populateUI()
+
 let ticketPrice = +movieSelect.value
+
+//Save selected movie index and price
+
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex)
+  localStorage.setItem('selectedMoviePrice', moviePrice)
+}
 
 // Updated select
 
 function updatedSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected')
+
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat))
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
   
   count.innerText = selectedSeats.length
   total.innerText = selectedSeats.length * ticketPrice
 }
 
+// Get data from localstore and populate UI
+
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'))
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected')
+      }
+    })
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex')
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex
+  }
+}
+
 // Movie select event
 
-movieSelect.addEventListener('change', e =>{
+movieSelect.addEventListener('change', e => {
   ticketPrice = +e.target.value
+  setMovieData(e.target.selectedIndex, e.target.value)
   updatedSelectedCount() 
 })
+
 // Seat select event
 container.addEventListener('click', e => {
   if (e.target.classList.contains('seat') && 
@@ -32,6 +66,10 @@ container.addEventListener('click', e => {
    updatedSelectedCount()
   }
 })
+
+// Initial count and total set
+
+updatedSelectedCount()
 
 
 
